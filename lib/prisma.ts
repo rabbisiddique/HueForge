da42daad74d07892a3f700a85c16d25d45a0d06c
SunnySiddique <devsunnywithcoding@gmail.com>
2025-11-05 18:21:46 +0600

    fixed prisma files

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
-// lib/prisma.ts
-import { PrismaClient } from "../lib/generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
+
 export default prisma;
